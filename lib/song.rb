1442fd18ee948c6cfd27e8f4129bb53220543be4
Jennifer Stoos <jstoos@bitlaw.com>
2017-12-09 18:19:33 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+require "pry"
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  # def name
+  #   @name
+  # end
+  # def name=(name)
+  #   @name = name
+  # end
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@unique_artists = []
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.each do |indiv_artist|
+      binding.pry
+      @@unique_artists.any? do |list_of_names|
+        list_of_names = indiv_artist
+        if false
+          @@unique_artists << indiv_artist
+        end
+      end
+    end
+    @@unique_artists
+  end
+
+end
