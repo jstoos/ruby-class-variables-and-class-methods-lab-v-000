b582df993d02b6ba9b07091a05c07934bddd6ad1
Jennifer Stoos <jstoos@bitlaw.com>
2017-12-09 22:16:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,8 +55,8 @@ class Song
         array = []
         @@genres.each do |individual_genre|
 
-          if individual_genre = unique_genre_item
-            then array << individual_genre
+          if individual_genre==unique_genre_item
+            array << individual_genre
           end
 
           @individual_genre = unique_genre_item
