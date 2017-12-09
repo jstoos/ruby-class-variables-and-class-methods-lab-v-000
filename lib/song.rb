b09eb53bb80217c57c3248772b540fe1529d56ab
Jennifer Stoos <jstoos@bitlaw.com>
2017-12-09 18:54:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
 
   def self.artists
-    binding.pry
+
     @@artists.each do |indiv_artist|
       if @@unique_artists.none? {|x| x = indiv_artist }
         @@unique_artists << indiv_artist
