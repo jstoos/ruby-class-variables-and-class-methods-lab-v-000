151344dcd20d42b4dfe655585127fa40306ac82b
Jennifer Stoos <jstoos@bitlaw.com>
2017-12-09 21:49:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,11 +54,10 @@ class Song
 
       @@unique_genre.reverse.each do |unique_genre_item, array=[]|
         @@genres.each do |individual_genre|
-          @individual_genre = individual_genre
           if individual_genre = unique_genre_item
             array << individual_genre
           end
-        array
+          @individual_genre = individual_genre
         end
         @@hash_final[@individual_genre]=array.count
       end
